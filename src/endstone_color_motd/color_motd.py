89fe506caa8d767a9a72714c841f05b891100809
FTS427 <FTS427@outlook.com>
2024-10-02 02:27:49 +0800

    T | test

--- src/endstone_color_motd/color_motd.py
+++ src/endstone_color_motd/color_motd.py
@@ -7,26 +7,22 @@ class ColorMotd(Plugin):
     def __init__(self):
         super().__init__()
         self.update_period: int = 20
-        self.first_motd: str = ""
-        self.second_motd: str = ""
-        self.third_motd: str = ""
+        self.motd: list[str] = []
+        self.i: int = 0
 
     def on_enable(self) -> None:
         self.save_default_config()
         self.load_config()
         self.register_events(self)
- 
+
     @event_handler
-    def change_motd(self, event: ServerListPingEvent) -> None:
-        event.motd = self.first_motd
-        self.server.scheduler.run_task(self, self.change_motd, delay = self.update_period)
-        event.motd = self.second_motd
-        self.server.scheduler.run_task(self, self.change_motd, delay = self.update_period)
-        event.motd = self.third_motd
-        self.server.scheduler.run_task(self, self.change_motd, delay = self.update_period)
+    def motd_update(self, event: ServerListPingEvent) -> None:
+        for i in len(self.motd):
+            event.motd = str(self.motd[i])
+            i+1
+        else:
+            i=0
     
     def load_config(self) -> None:
         self.update_period = self.config["update_period"]
-        self.first_motd = self.config["first_motd"]
-        self.second_motd = self.config["second_motd"]
-        self.third_motd = self.config["third_motd"]
+        self.motd = self.config["motd"]
